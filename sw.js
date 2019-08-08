70c24600f1e31c1b19de186c84ce26709de91cb7
laura <laura@visulytix.com>
2019-08-08 17:20:41 +0100

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-104697fa2092b7a11ffa.js"
+    "url": "webpack-runtime-f1bc46727b76a82d04bd.js"
   },
   {
     "url": "styles.5f81339daa9828444137.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1a9ffa26d4709affd02f40dccc3f2c19"
+    "revision": "ab919cb613a33a7bcc28add3d324a09d"
   },
   {
     "url": "component---src-pages-404-js-621c5ea781895cafdce3.js"
@@ -52,7 +52,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "cc779b596c0535f390f7a1f9ac1dbf3e"
+    "revision": "e15cee08414ec04060d7a3f157c5bded"
   },
   {
     "url": "manifest.webmanifest",
